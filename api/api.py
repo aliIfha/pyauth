48edb52e1663875d62eb7265ed69aff0eb7d28e4
AliA <62218294+aliisverymad@users.noreply.github.com>
2022-01-06 23:56:40 +0000

    Loads of progress | Major Update
    I finally picked up the energy to work on pyauth and added loads.
    
    I have MFA semi-intergrated into the API since it isn't finished yet but will work flawlessly when it's finished. The module I'm using for authenticator codes is PyOTP.
    
    Added the foundation for several features which all can be changed extensivley in the apiconfig.json. I've add an API connection key option so that only applications you approve with your key can use the API, all configarable and able to be enabled or disabled if you want. I've also added the following features: Enforced MFA (accounts must have MFA enabled to function), Location Verification, Anti-VPN filter, API Key only mode (used to lock registration to people who have a controlled distributed unique key, this allows for monetisation.), Identifier Mode (Used for configuring wether to use an email or a username or both.), Webhook Notifications (used to send notifications through a webhook).
    
    I have loads to add and I'm considiring add email verification to MFA but that's low priority as i haven't even got this thing running properly yet.
    
    I've started writing the actual library which contains a probe feature to ask any pyauth API what features are enabled and disabled (you can configure the API to not give out infomation to clients without a key). I've wrote a register definition which works and can send an username and password (which gets hashed before it gets sent) to a compatible pyauth server. Written status codes to signal what's happened to a request. I have started writing an login definition but it's not completely finished as I need to finish backend before starting the library.
    
    I'm considering moving PostgreSQL commands to another module to make the code more organised and clean. This will also help me when I write support for other database types. If I do this, I will move colorama to another module just to make it easier for me.
    
    Anyways this description has been going on for wayy too long.
    
    This commit of pyauth is currently semi=functional.

--- api/api.py
+++ api/api.py
@@ -9,9 +9,10 @@
 
 # Start the very basic of libraries
 import os
+from types import MethodDescriptorType
 print("Initalising Pyauth")
 print("Please wait...")
-version = "1.0.0"
+version = "prerelease"
 
 def logo():
     command = 'clear'
@@ -70,23 +71,48 @@ from datetime import datetime
 import hashlib
 import json
 import re
+import pyotp
 
 # Load main API settings from JSON file into memory
 with open('apiconfig.json') as apiconfig:
     config = json.load(apiconfig)
     masterapikey = config.get('masterapikey')
+    useAPIconnectionkey = config.get('useAPIconnectionkey')
+    if useAPIconnectionkey == True:
+        apiconnectionkey = config.get('APIconnectionkey')
+    else:
+        pass
     if masterapikey == "defaultkey":
         logo()
         print('[','\033[31mERROR','\033[0m]: Insecure master key warning!')
         print('[','\033[34mINFO','\033[0m]: Please change the master API key from default in apiconfig.json.')
         print('[','\033[34mINFO','\033[0m]: This prevents from bad actors from executing actions locked to the admin!')
         print('[','\033[34mINFO','\033[0m]: Pyauth will not launch until the key is changed')
+        apiconfig.close()
+        exit()
+    elif apiconnectionkey == "defaultkey":
+        logo()
+        print('[','\033[31mERROR','\033[0m]: Insecure master key warning!')
+        print('[','\033[34mINFO','\033[0m]: Please change the master API key from default in apiconfig.json.')
+        print('[','\033[34mINFO','\033[0m]: This prevents from bad actors from executing actions locked to the admin!')
+        print('[','\033[34mINFO','\033[0m]: Pyauth will not launch until the key is changed')
+        apiconfig.close()
         exit()
     else:
         pass
     usehwid = config.get('usehwidverification')
     allowoutdated = config.get('allowoutdatedconnections')
     allow2fa = config.get('use2FAverification')
+    enforce2fa = config.get('enforce2FAverification')
+    useGPS = config.get('useGPS')
+    antiVPN = config.get('useantivpn')
+    apikeyonly = config.get('apikeyonly')
+    identifiermode = config.get('identifiermode')
+    webhookenabled = config.get('webhookenabled')
+    if webhookenabled == True:
+        webhookurl = config.get('webhookurl')
+    else:
+        pass
     apiconfig.close()
 
 # Load main PostgreSQL location from JSON file into memory
@@ -105,50 +131,47 @@ if dbmode == "postgres":
     db = psycopg2.connect(database=database, user=username, password=password, host=hostname, port=dbport)
     cur = db.cursor()
     # Check if Postgres DB's exist
-    cur.execute('''select exists(SELECT datname FROM pg_catalog.pg_database WHERE lower(datname) = lower('paUsers'));''')
-    userdbcheck = cur.fetchall()
-    cur.execute('''select exists(SELECT datname FROM pg_catalog.pg_database WHERE lower(datname) = lower('paKeys'));''')
-    keydbcheck = cur.fetchall()
+    cur.execute("SELECT datname FROM pg_database;")
+    list_database = cur.fetchall()
+    print(list_database)
     # If they do, continue. if they don't, error and create a DB and then continue
-    for row in userdbcheck:
-        print(row[0])
-        if row[0] == True:
-            print('[','\033[32mOK','\033[0m]: User authentication database exists!')
-        elif row[0] == False:
-            print('[','\033[31mERROR','\033[0m]: User authentication database not found!')
-            print('[','\033[34mINFO','\033[0m]: Creating database...')
-            cur.execute('''
-    CREATE TABLE paUsers (
-        apikey TEXT UNIQUE NOT NULL,
-        email VARCHAR ( 255 ) UNIQUE NOT NULL,
-        username VARCHAR ( 50 ) UNIQUE NOT NULL,
-        password TEXT NOT NULL,
-        tiedhwid TEXT UNIQUE,
-        token TEXT UNIQUE NOT NULL,
-        created_on TIMESTAMP,
-        last_login TIMESTAMP
-        );''')
-            db.commit()
-            print('[','\033[32mOK','\033[0m]: Database created successfully!')
-        else:
-            print('[','\033[31mERROR','\033[0m]: A internal error occured...')
-            exit()
-    for row in keydbcheck:
-        if row[0] == True:
-            print('[','\033[32mOK','\033[0m]: API Key database exists!')
-        elif row[0] == False:
-            print('[','\033[31mERROR','\033[0m]: API Key database not found!')
-            print('[','\033[34mINFO','\033[0m]: Creating database...')
-            cur.execute('''
-    CREATE TABLE paKeys (
-        apikey VARCHAR ( 255 ) UNIQUE NOT NULL,
-        keytype VARCHAR ( 50 ) NOT NULL,
-        created_on TIMESTAMP);''')
-            db.commit()
-            print('[','\033[32mOK','\033[0m]: Database created successfully!')
-        else:
-            print('[','\033[31mERROR','\033[0m]: A internal error occured...')
-            exit()
+    if "paUsers" in list_database:
+        print('[','\033[32mOK','\033[0m]: User authentication database exists!')
+    elif "paUsers" not in list_database:
+        print('[','\033[31mERROR','\033[0m]: User authentication database not found!')
+        print('[','\033[34mINFO','\033[0m]: Creating database...')
+        cur.execute('''
+CREATE TABLE IF NOT EXISTS paUsers (
+    apikey TEXT UNIQUE,
+    email VARCHAR ( 255 ) UNIQUE,
+    username VARCHAR ( 50 ) UNIQUE,
+    password TEXT NOT NULL ,
+    mfasecret TEXT,
+    tiedhwid TEXT UNIQUE,
+    token TEXT UNIQUE NOT NULL,
+    created_on TIMESTAMP,
+    last_login TIMESTAMP
+    );''')
+        db.commit()
+        print('[','\033[32mOK','\033[0m]: Database created successfully!')
+    else:
+        print('[','\033[31mERROR','\033[0m]: A internal error occured...')
+        exit()
+    if "paKeys" in list_database:
+        print('[','\033[32mOK','\033[0m]: API Key database exists!')
+    elif "paKeys" not in list_database:
+        print('[','\033[31mERROR','\033[0m]: API Key database not found!')
+        print('[','\033[34mINFO','\033[0m]: Creating database...')
+        cur.execute('''
+CREATE TABLE IF NOT EXISTS paKeys (
+    apikey VARCHAR ( 255 ) UNIQUE NOT NULL,
+    keytype VARCHAR ( 50 ) NOT NULL,
+    created_on TIMESTAMP);''')
+        db.commit()
+        print('[','\033[32mOK','\033[0m]: Database created successfully!')
+    else:
+        print('[','\033[31mERROR','\033[0m]: A internal error occured...')
+        exit()
     os.system('pause')
 else:
     logo()
@@ -163,12 +186,44 @@ app = Flask(__name__)
 def index():
     return 'API ONLINE', 200
 
+# Admin API routes. please for the love of this API, don't share your master api key with anyone (apart from me of course :3).
+@app.route('/api/v2/internal/createapikey', methods=['GET', 'POST'])
+def createapikey():
+    masterapikey = 'e46d0ec5-c753-4ef9-997f-0f2d592b7e23'
+    #masterapikey = os.getenv("apikey")
+    inputkey = request.headers.get('Authorization')
+    if inputkey == masterapikey:
+        apikey = secrets.token_urlsafe(64)
+        return jsonify({"apikey" : apikey})
+    else:
+        return 'NOT ALLOWED', 405
+
+
+# API user authentication routes. This is where the actual internals of the API lay.
 @app.route('/api/v2/auth/info', methods=['GET', 'POST'])
 def serverinfo():
-    return jsonify({"usehwid" : f"{usehwid}", "allowoutdated": f"{allowoutdated}", "allow2fa": f"{allow2fa}"})
+    return jsonify({"version" : f"{version}", "usehwid" : f"{usehwid}", "allowoutdated": f"{allowoutdated}", "allow2fa": f"{allow2fa}", "enforce2fa": f"{enforce2fa}", "useGPS": f"{useGPS}", "antiVPN" : f"{antiVPN}"}), 200
+
+@app.route('/api/v2/user/register', methods=['GET', 'POST'])
+def register():
+    totp = pyotp.TOTP(pyotp.random_base32())
+    apikey = request.headers.get('username')
+    email = ""
+    username = request.headers.get('username')
+    password = request.headers.get('password')
+    token = secrets.token_urlsafe(128)
+    date = datetime.today().strftime('%Y-%m-%d')
+    if usehwid == True:
+        hwid = request.headers.get('hwid')
+    cur.execute(f"INSERT INTO userdetails (apikey,email,username,password,tiedhwid,created_on,last_login,token) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);", (apikey, email, username, password, hwid, date, date, token))
+    return jsonify({"mfasecretkey" : f"{totp}"}), 201
+
+@app.route('/api/v2/user/login', methods=['GET', 'POST'])
+def auth():
+    pass # work in progress
 
 
 logo()
 print("Press CTRL+C to quit")
-app.run(debug=True, use_reloader=False)
+app.run(debug=True, use_reloader=True)
     
---
+++ library/test.py
@@ -0,0 +1,5 @@
+import main
+
+apiURL = "http://127.0.0.1:5000/"
+
+main.probe(apiURL)
